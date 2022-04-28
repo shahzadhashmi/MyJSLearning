09b5d43b41dc6bead175ee6e0b21e15e444845ad
Shahzad Hashmi <Shashmi154@gmail.com>
2022-04-28 14:49:18 +0500

    addition of info method in original file

--- 03Intermediate/11Object.js
+++ 03Intermediate/11Object.js
@@ -2,7 +2,11 @@ var user = {
     fullName: 'Shahzad',
     phoneNumber: '0321455555',
     address: 'abc street',
+    facebookSignedIn: true,
+    role: 'admin',
+    loginCount: 10,
     courseList: [],
+    courseNumber: [],
     // we can also use method inside the object.
     buyCourse: function (courseName) {
         // we'll use this keyword here to access the object value inside the method
@@ -14,9 +18,15 @@ var user = {
     getCourseCount: function () {
         return `${this.fullName} is enrolled in ${this.courseList.length} courses`;
     },
+    info: function (courseNumber) {
+       return  `${this.fullName} is enrolled in ${this.courseList.length} courses and he is loggedIn form ${this.facebookSignedIn}. His total loginCount is ${this.loginCount}.`
+    }
+
 };
 
 console.log(user.fullName);
 console.log(user.getCourseCount());
 user.buyCourse("Angular 10");
 console.log(user.getCourseCount());
+console.log(user.getCourseCount());
+console.log(user.info());
