7d3b80c9ae0f583563469368931b96b0f589dfc7
Shahzad Hashmi <Shashmi154@gmail.com>
2022-05-01 00:17:56 +0500

    array question answer addition in previous file

--- 03Intermediate/13ForLoop.js
+++ 03Intermediate/13ForLoop.js
@@ -73,12 +73,13 @@ var myCities = [
 
 //}
 
-for (let i = 0; i <= myCities.length; i++) {
-    console.log(myCities[i]);
-} 
+// for (let i = 0; i < myCities.length; i++) {
+//     if (typeof myCities[i] === 'string') continue;
+//     console.log(myCities[i]);
+// } 
 
 /* 
-output: notice one thing about this logic we have undefined here that is because we are adding "=" i <= myCities.length; in r condition. so don't use = here.
+output: notice one thing about this logic we have undefined here that is because we are adding "=" i <= myCities.length; in r condition. so don't use = 
 Isb
 rwp
 balochistan 
@@ -88,3 +89,16 @@ muzaffarabad
 true
 undefined
 */
+
+
+/* Question# 1 What will be the output? */
+const array1 = [5,8,"012", 130, 144]
+/* find method return the first element that satisfies the condition 
+    in the callback. if the condition is not satisfied then it will return undefined.
+    in the given question "012" is converted to number(12) it is the first element that satisfies
+    the condition in the callback. so the output will be (012).
+*/
+const found = array1.find(element => element > 11);
+// const found = array1.find(element => element > 144);
+// output : undefined
+console.log(found);
